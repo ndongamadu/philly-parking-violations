2d0083b55265ff34a7e9d975ff53fcd5a63d06bd
Nick Hand <nhand@berkeley.edu>
2018-08-25 22:25:32 -0400

    update to 2017

--- app.py
+++ app.py
@@ -9,23 +9,27 @@ curr_dir = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/data")
 def get_data():
 
     # get the year and month of data to load
-    year = request.args.get('year', default = 2016, type = int)
-    month = request.args.get('month', default = 1, type = int)
+    year = request.args.get('year', default=2017, type=int)
+    month = request.args.get('month', default=1, type=int)
 
     # read the parking violation json data
-    filename = os.path.join(curr_dir, 'data', 'parking_violations_%d_%d.json.gz' %(month, year))
+    filename = os.path.join(
+        curr_dir, 'data', 'parking_violations_%d_%d.json.gz' % (month, year))
     df = pd.read_json(filename, convert_dates=False)
 
     # return data
     return df.to_json(orient='records')
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
